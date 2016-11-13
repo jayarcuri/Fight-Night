9e5e7a85b4a41a3a4ccb5cb1da61850f3df9fd63
Adam Mullarkey <acmullarkey@gmail.com>
2016-11-12 19:53:08 -0500

    Added Game Timer to UI.

---
+++ NightFight/Assets/GameTimer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameTimer : MonoBehaviour {
+	public int roundLengthInSeconds;
+	public GameObject victoryScreen;
+
+	Text timerText;
+	float roundStartTime;
+	int secondsLeftInRound;
+
+	// Use this for initialization
+	void Start () {		
+		timerText = GetComponent<Text> ();
+		SetUpTimer ();
+	}
+	
+	public void UpdateTimer () {
+		float currentTime = Time.time;
+		int secondsSinceRoundHasStarted = Mathf.RoundToInt (currentTime - roundStartTime);
+
+		secondsLeftInRound = roundLengthInSeconds - secondsSinceRoundHasStarted;
+
+		timerText.text = secondsLeftInRound.ToString().PadLeft(2, '0');
+
+		if (secondsLeftInRound <= 0) {
+			//	TODO: tigger game over screen (with Time Over)
+			CancelInvoke ();
+		}
+	}
+
+	public void SetUpTimer () {
+		timerText.text = roundLengthInSeconds.ToString ();
+		roundStartTime = Time.time;
+		InvokeRepeating ("UpdateTimer", 0f, 1f); 
+	}
+}
